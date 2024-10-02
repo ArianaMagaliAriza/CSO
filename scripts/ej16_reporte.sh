ca184fc97d1ecded8a45ebe457198295e45906c7
Ariana Magali Ariza <arianamagaliariza@gmail.com>
2024-10-02 18:00:49 -0300

    Update ej16_reporte.sh

--- scripts/ej16_reporte.sh
+++ scripts/ej16_reporte.sh
@@ -11,5 +11,7 @@ fi
 for linea in $(cat /etc/passwd | cut -d: -f 1,6); do
     user=$(echo $linea | cut -d: -f1)
     home=$(echo $linea | cut -d: -f2)
-    echo "Usuario: $user - Home: $home" 
+    # echo "Usuario: $user - Home: $home"
+    if [ -d $home ]; then
+         find $home -name "*.$1" 
     done
